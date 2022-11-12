3b299838f5f2b1c69de454258a38ee2c0eeb1561
Ngô Quốc Đạt <datlechin@gmail.com>
2022-11-12 17:10:34 +0700

    chore: update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,15 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import NProgress from 'nprogress'
 import { Inertia } from '@inertiajs/inertia'
 
-import '../sass/app.scss';
+import '../sass/app.scss'
 
-import Layout from "@/components/Layout.vue";
+import Layout from "@/components/Layout.vue"
 
-createInertiaApp({
+await createInertiaApp({
     resolve: (name) => {
         const page = resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue'))
         page.then((module) => {
@@ -19,7 +19,7 @@ createInertiaApp({
         return page
     },
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+        const app = createApp({ render: () => h(App, props) })
             .use(plugin)
             .mount(el)
     },
