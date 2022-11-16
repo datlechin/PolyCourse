690f60a287785944e4511922e2cd6e693201dc08
Ngô Quốc Đạt <datlechin@gmail.com>
2022-11-16 20:58:53 +0700

    wip

---
+++ resources/js/components/SocialLoginList.vue
@@ -0,0 +1,51 @@
+<script setup>
+import FacebookIcon from "@/icons/FacebookIcon.vue";
+import TwitterIcon from "@/icons/TwitterIcon.vue";
+import GithubIcon from "@/icons/GithubIcon.vue";
+
+const socials = [
+    {
+        name: 'Facebook',
+        url: null,
+        icon: FacebookIcon,
+        color: 'text-blue-600'
+    },
+    {
+        name: 'Twitter',
+        url: null,
+        icon: TwitterIcon,
+        color: 'text-blue-500'
+    },
+    {
+        name: 'Github',
+        url: null,
+        icon: GithubIcon,
+        color: 'text-gray-900'
+    }
+];
+</script>
+
+<template>
+    <div class="mt-6">
+        <div class="relative">
+            <div class="absolute inset-0 flex items-center">
+                <div class="w-full border-t border-gray-300"></div>
+            </div>
+            <div class="relative flex justify-center text-sm">
+                <span class="bg-white px-2 text-gray-500">Hoặc đăng nhập bằng</span>
+            </div>
+        </div>
+
+        <div class="mt-6 grid grid-cols-3 gap-3">
+            <div
+                v-for="(social, index) in socials"
+                :key="index"
+            >
+                <a :href="social.url" class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50">
+                    <span class="sr-only">Đăng nhập bằng {{ social.name }}</span>
+                    <component :is="social.icon" :class="['w-5 h-5', social.color]" />
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
