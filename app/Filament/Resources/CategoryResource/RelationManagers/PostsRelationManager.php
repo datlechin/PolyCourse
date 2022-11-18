5cd65840c6852c8074f35ae1e83dd5b9f8692405
Ngô Quốc Đạt <datlechin@gmail.com>
2022-11-18 15:11:22 +0700

    improve CategoryResource (#9)
    * feat: courses list page
    
    * wip
    
    * wip
    
    * improve CategoryResource

--- app/Filament/Resources/CategoryResource/RelationManagers/PostsRelationManager.php
+++ app/Filament/Resources/CategoryResource/RelationManagers/PostsRelationManager.php
@@ -10,6 +10,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Resources\Table;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Illuminate\Support\Str;
 
@@ -17,76 +18,28 @@ class PostsRelationManager extends RelationManager
 {
     protected static string $relationship = 'posts';
 
-    protected static ?string $recordTitleAttribute = 'title';
-
     protected static ?string $label = 'Bài viết';
-
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Select::make('category_id')
-                    ->relationship('category', 'name')
-                    ->searchable()
-                    ->required(),
-
-                Select::make('user_id')
-                    ->relationship('user', 'name')
-                    ->searchable()
-                    ->required(),
-
-                TextInput::make('title')
-                    ->required()
-                    ->reactive()
-                    ->afterStateUpdated(fn ($state, callable $set) => $set('slug', Str::slug($state))),
-
-                TextInput::make('slug')
-                    ->disabled()
-                    ->required()
-                    ->unique(Post::class, 'slug', fn ($record) => $record),
-
-                TextInput::make('excerpt'),
-
-                MarkdownEditor::make('content')
-                    ->required(),
-
-                TextInput::make('views')
-                    ->required()
-                    ->integer(),
-
-                Placeholder::make('created_at')
-                    ->label('Created Date')
-                    ->content(fn (?Post $record): string => $record?->created_at?->diffForHumans() ?? '-'),
-
-                Placeholder::make('updated_at')
-                    ->label('Last Modified Date')
-                    ->content(fn (?Post $record): string => $record?->updated_at?->diffForHumans() ?? '-'),
-            ]);
-    }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                TextColumn::make('category.name')
-                    ->searchable()
-                    ->sortable(),
+                SpatieMediaLibraryImageColumn::make('thumbnail')
+                    ->label('Hình ảnh'),
 
                 TextColumn::make('user.name')
+                    ->label('Người ')
                     ->searchable()
                     ->sortable(),
 
                 TextColumn::make('title')
+                    ->label('Tiêu đề')
                     ->searchable()
                     ->sortable(),
 
-                TextColumn::make('slug')
-                    ->searchable()
-                    ->sortable(),
-
-                TextColumn::make('excerpt'),
-
-                TextColumn::make('views'),
+                TextColumn::make('views')
+                    ->label('Lượt xem')
+                    ->formatStateUsing(fn (string $state): string => number_format($state))
             ]);
     }
 }
