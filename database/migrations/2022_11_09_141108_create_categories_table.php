79483684238927889a56fce5061d21060a1be5fb
Ngo Quoc Dat <datlechin@gmail.com>
2022-11-22 10:50:06 +0700

    chore: category description length

--- database/migrations/2022_11_09_141108_create_categories_table.php
+++ database/migrations/2022_11_09_141108_create_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
-            $table->string('description')->nullable();
+            $table->string('description', 300)->nullable();
             $table->timestamps();
         });
     }
