685136ef28866726c4ac0b7795f7576d06381ac7
Ngô Quốc Đạt <datlechin@gmail.com>
2022-11-27 00:05:15 +0700

    feat: scene learning (#19)
    * feat: scene learning
    
    * update

---
+++ resources/js/Pages/Learning.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { defineLayout, Link } from '@inertiajs/vue3'
+import { PlayCircleIcon } from '@heroicons/vue/24/solid'
+import LearningLayout from '@/components/LearningLayout.vue'
+import route from 'ziggy-js/src/js'
+import { secondsToTime, dateFormat } from '@/helpers'
+
+defineLayout(LearningLayout)
+
+defineProps({
+    course: Object,
+})
+</script>
+
+<template>
+    <div class="grid grid-cols-4">
+        <div class="col-span-1">
+            <div class="py-3 px-5 font-semibold">
+                Nội dung khoá học
+            </div>
+            <ul class="overflow-y-auto h-screen scrollbar">
+                <li
+                    v-for="(lesson, index) in course.lessons"
+                    :key="index"
+                    class="py-3 px-5 hover:bg-gray-200"
+                >
+                    <Link :href="route('learning', { course: course.slug, lesson: lesson.id})">
+                        <div>
+                            {{ index + 1 }}.
+                            {{ lesson.name }}
+                        </div>
+                        <div class="flex items-center text-gray-600">
+                            <PlayCircleIcon class="w-3 h-3 mr-1" />
+                            <span class="text-sm">{{ secondsToTime(lesson.time_duration) }}</span>
+                        </div>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+        <div class="col-span-3">
+            <iframe :src="course.lesson.youtube_url" height="80%" width="100%" />
+            <div class="mx-20 my-8">
+                <h1 class="font-bold text-3xl">{{ course.lesson.name }}</h1>
+                <div class="text-sm text-gray-600 mt-2">Cập nhật {{ dateFormat(course.lesson.updated_at) }}</div>
+                <div class="prose mt-8">
+                    {{ course.lesson.content }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.scrollbar::-webkit-scrollbar {
+    width: 8px;
+}
+
+.scrollbar::-webkit-scrollbar-thumb {
+    @apply bg-gray-400 rounded-lg;
+}
+
+.scrollbar::-webkit-scrollbar-thumb:hover {
+    background: #c0a0b9;
+}
+</style>
---
+++ resources/js/components/LearningLayout.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import { ChevronLeftIcon } from '@heroicons/vue/24/solid'
+import route from "ziggy-js/src/js";
+defineProps({
+    course: Object,
+})
+</script>
+
+<template>
+    <header class="sticky top-0 flex justify-between items-center bg-gray-800 h-12 px-7">
+        <div class="flex items-center">
+            <Link :href="route('home')" class="mr-7">
+                <ChevronLeftIcon class="text-white w-5 h-5" />
+            </Link>
+            <div class="flex items-center">
+                <img src="@/../images/logo.png" alt="" class="w-8 h-8 rounded-lg">
+                <span class="text-white font-bold ml-3" v-text="course.name" />
+            </div>
+        </div>
+    </header>
+    <main>
+        <slot />
+    </main>
+
+    <footer>
+
+    </footer>
+</template>
