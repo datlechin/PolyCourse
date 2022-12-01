17fb804d463b41467610c82f67f925d552a7a0ef
Ngô Quốc Đạt <datlechin@gmail.com>
2022-12-01 20:41:38 +0700

    feat: admin user resource (#28)

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,12 +11,15 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class User extends Authenticatable implements FilamentUser, HasAvatar
+class User extends Authenticatable implements FilamentUser, HasAvatar, HasMedia
 {
     use HasApiTokens;
     use HasFactory;
     use Notifiable;
+    use InteractsWithMedia;
 
     /**
      * The attributes that are mass assignable.
