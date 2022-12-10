c0802d83c9a958fbba0a7b0f8fb05f2f163ef3fe
Pham Ngoc Dat <pndat171203@gmail.com>
2022-12-10 20:32:22 +0700

    chore: hide filament logo in admin

--- config/filament.php
+++ config/filament.php
@@ -140,7 +140,7 @@ return [
         'path' => app_path('Filament/Widgets'),
         'register' => [
             Widgets\AccountWidget::class,
-            Widgets\FilamentInfoWidget::class,
+            // Widgets\FilamentInfoWidget::class,
         ],
     ],
 
@@ -236,7 +236,7 @@ return [
             'have_inline_labels' => false,
         ],
         'footer' => [
-            'should_show_logo' => true,
+            'should_show_logo' => false,
         ],
         'max_content_width' => null,
         'notifications' => [
