3b4fdeb10b261299ea3c0778b1b9c919c584ea9a
Ngô Quốc Đạt <datlechin@gmail.com>
2022-12-17 14:09:01 +0700

    feat: admin table widgets (#59)

--- config/filament.php
+++ config/filament.php
@@ -139,7 +139,7 @@ return [
         'namespace' => 'App\\Filament\\Widgets',
         'path' => app_path('Filament/Widgets'),
         'register' => [
-            Widgets\AccountWidget::class,
+            // Widgets\AccountWidget::class,
             // Widgets\FilamentInfoWidget::class,
         ],
     ],
